7bc8e433f8041bf3350a58c1cf6468ecb8f5f5b6
Mathieu Duchesneau <duchesneau.mathieu@gmail.com>
2018-11-30 09:08:27 -0500

    constant zero init to bias

--- models/base.py
+++ models/base.py
@@ -3,7 +3,6 @@ from torch import nn
 
 def MLP(struct, non_linearity=nn.ReLU, batch_norm=True, w_bound=None):
     w_bound = w_bound**.5
-    print(w_bound)
     layers = list()
     for ni, no in zip(struct[:-2], struct[1:-1]):
         if batch_norm:
@@ -13,10 +12,12 @@ def MLP(struct, non_linearity=nn.ReLU, batch_norm=True, w_bound=None):
             layers.append(non_linearity())
         else:
             l = nn.Linear(ni, no)
+            nn.init.constant_(l.bias, 0)
             layers.append(l)
             layers.append(non_linearity())
         if w_bound is not None: nn.init.uniform_(l.weight, -w_bound, w_bound)
     l = nn.Linear(struct[-2], struct[-1])
+    nn.init.constant_(l.bias, 0)
     if w_bound is not None: nn.init.uniform_(l.weight, -w_bound, w_bound)
     layers.append(l)
     return nn.Sequential(*layers)
